f8e23aa78c53bd2130acf28602fc848c55337eb7
Deekshithapshetty <91956453+Deekshithapshetty@users.noreply.github.com>
2024-08-21 20:31:42 +0530

    Update LoginService.js

--- food-ordering-app/food-ordering-front/src/services/LoginService.js
+++ food-ordering-app/food-ordering-front/src/services/LoginService.js
@@ -3,12 +3,12 @@ import TokenService from "./TokenService";
 
 class LoginService{
     login(loginParams){
-        return axios.post("http://3.235.104.196:8080/api/login", loginParams);
+        return axios.post("http://3.230.3.253:8080/api/login", loginParams);
     }
 
     logout(){
         TokenService.setTokenInHeader();
-        return axios.get("http://3.235.104.196:8080/api/logout");
+        return axios.get("http://3.230.3.253:8080/api/logout");
     }
 }
 
