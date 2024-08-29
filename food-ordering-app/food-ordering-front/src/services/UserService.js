2933773f184a46c84b1e3da180bd9c939eaaf8fd
Deekshithapshetty <91956453+Deekshithapshetty@users.noreply.github.com>
2024-08-29 20:44:31 +0530

    Update UserService.js

--- food-ordering-app/food-ordering-front/src/services/UserService.js
+++ food-ordering-app/food-ordering-front/src/services/UserService.js
@@ -6,51 +6,51 @@ class UserService{
 
     getAllEmployees () {
         TokenService.setTokenInHeader();
-        return axios.get("http://34.229.83.122:8080/api/user/getAllEmployees");
+        return axios.get("http://54.160.249.249:8080/api/user/getAllEmployees");
     }
 
     getAllUsers(){
         TokenService.setTokenInHeader();
-        return axios.get("http://34.229.83.122:8080/api/user/getAllUsers");
+        return axios.get("http://54.160.249.249:8080/api/user/getAllUsers");
     }
 
     createEmployee(user){
         TokenService.setTokenInHeader();
-        return axios.post("http://34.229.83.122:8080/api/user/createEmployee", user);
+        return axios.post("http://54.160.249.249:8080/api/user/createEmployee", user);
     }
 
     getEmployeeById(employeeId){
         TokenService.setTokenInHeader();
-        return axios.get("http://34.229.83.122:8080/api/user/" + employeeId);
+        return axios.get("http://54.160.249.249:8080/api/user/" + employeeId);
     }
     //moze i samo employee da se salje, ne mora i id
     updateEmployee(employeeId, employee){
         TokenService.setTokenInHeader();
-        return axios.put("http://34.229.83.122:8080/api/user/updateUserByIdAndDetails/" + employeeId, employee);
+        return axios.put("http://54.160.249.249:8080/api/user/updateUserByIdAndDetails/" + employeeId, employee);
     }
     //logicko brisanje, setuje se isDeleted na true
     deleteEmployee(employeeId){
         TokenService.setTokenInHeader();
-        return axios.put("http://34.229.83.122:8080/api/user/deactivateUser/" + employeeId);
+        return axios.put("http://54.160.249.249:8080/api/user/deactivateUser/" + employeeId);
     }
 
     registration(user){
-        return axios.post("http://34.229.83.122:8080/api/user/registration", user);
+        return axios.post("http://54.160.249.249:8080/api/user/registration", user);
     }
 
     getCurrentUser(){
         TokenService.setTokenInHeader();
-        return axios.get("http://34.229.83.122:8080/api/user/getCurrentUser");
+        return axios.get("http://54.160.249.249:8080/api/user/getCurrentUser");
     }
 
     updateUser(user){
         TokenService.setTokenInHeader();
-        return axios.put("http://34.229.83.122:8080/api/user/updateUser", user);
+        return axios.put("http://54.160.249.249:8080/api/user/updateUser", user);
     }
 
     changePassword(passwordObj){
         TokenService.setTokenInHeader();
-        return axios.put("http://34.229.83.122:8080/api/user/changePassword", passwordObj);
+        return axios.put("http://54.160.249.249:8080/api/user/changePassword", passwordObj);
     }
 
 }
